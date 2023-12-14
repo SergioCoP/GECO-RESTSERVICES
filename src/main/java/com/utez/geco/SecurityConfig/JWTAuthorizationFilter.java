a13268794c34db410e509f4082467130d08b0116
20203tn125 <20203tn125@utez.edu.mx>
2023-12-14 13:52:33 -0600

    fix ya por favor

--- src/main/java/com/utez/geco/SecurityConfig/JWTAuthorizationFilter.java
+++ src/main/java/com/utez/geco/SecurityConfig/JWTAuthorizationFilter.java
@@ -26,7 +26,12 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
             UsernamePasswordAuthenticationToken usernamePAT = TokenUtils.getToken(token);
             SecurityContextHolder.getContext().setAuthentication(usernamePAT);
         }
-
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD");
+        response.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        response.addHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Credentials");
+        response.addHeader("Access-Control-Allow-Credentials", "true");
+        response.addIntHeader("Access-Control-Max-Age", 10);
         filterChain.doFilter(request, response);
     }
 }
