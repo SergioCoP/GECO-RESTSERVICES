27a526733d2a29edd462357d418b8587a5efd9ff
20203tn125 <20203tn125@utez.edu.mx>
2023-12-13 23:48:30 -0600

    [UPDATE] Configuracion cors y reimplementacion servicios v1.2

--- src/main/java/geco/app/service/utils/securityConfig/WebSecurityConfig.java
+++ src/main/java/geco/app/service/utils/securityConfig/WebSecurityConfig.java
@@ -105,6 +105,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.header.writers.StaticHeadersWriter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -114,9 +115,9 @@ import javax.sql.DataSource;
 import java.util.Arrays;
 
 @Configuration
-@EnableMethodSecurity
 @EnableWebSecurity
 @RequiredArgsConstructor
+@CrossOrigin
 public class WebSecurityConfig {
     private final UserDetailsService userDetailsService;
     //private final JWTAuthorizationFilter jwtAuthorizationFilter;
