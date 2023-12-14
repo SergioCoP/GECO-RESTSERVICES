52fd6e55dd2915ce7b9e9a7c15b0f030c93bf215
20203tn125 <20203tn125@utez.edu.mx>
2023-12-14 11:27:43 -0600

    fix d

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,3 @@
-insert into rol(name,description) values('ROLE_GERENTE','Gerente'),('ROLE_SUPERVISOR','Recepcionista'),('ROLE_PERSONAL','Personal de limpieza');
+insert into rol (description,name) values ('Gerente','ROLE_GERENTE'),('Recepcionista','ROLE_RECEPCIONISTA'),('Personal de limpieza','ROLE_LIMPIEZA');
 insert into person(name,surname,lastname) values('Sergio','Cortes','Popoca');
-insert into user(id_person,email,password) values(1,'sergiocortes518@gmail.com','$2a$10$DsLoOlL0q4Swr9eNz20HeuaDTbraVfe90OKsGjJir/t7Dpuh55sZ2');#password: #Serch1234
-insert into user_rol(id_rol,id_user) values(1,1);
+insert into user(id_person,id_rol,email,password,username) values(1,1,'sergiocortes518@gmail.com','$2a$10$DsLoOlL0q4Swr9eNz20HeuaDTbraVfe90OKsGjJir/t7Dpuh55sZ2','serch21');
