97d9c56129845d12b884d262bd2931356681ad27
20203tn125 <20203tn125@utez.edu.mx>
2023-12-14 00:34:31 -0600

    [UPDATE] Configuracion cors y reimplementacion servicios v1.9

--- src/main/java/geco/app/service/utils/firebase/FirebaseController.java
+++ src/main/java/geco/app/service/utils/firebase/FirebaseController.java
@@ -5,10 +5,7 @@ import com.google.cloud.storage.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
@@ -16,7 +13,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 
-@Controller
+@RestController
 @RequestMapping("/api/image-upload")
 @CrossOrigin("*")
 public class FirebaseController {
