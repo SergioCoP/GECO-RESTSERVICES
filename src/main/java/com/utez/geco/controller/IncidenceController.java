6c0f8e8d3ec9ebae9afeacba15659e7862b5da76
20203tn125 <20203tn125@utez.edu.mx>
2023-12-15 08:13:43 -0600

    fix

--- src/main/java/com/utez/geco/controller/IncidenceController.java
+++ src/main/java/com/utez/geco/controller/IncidenceController.java
@@ -120,6 +120,7 @@ public class IncidenceController {
                 svIncDTO.getIdRoom()
         );
 
+        System.out.println(incidence.getIdUser().getIdUser());
         usr = us.findById(incidence.getIdUser().getIdUser()) == null;
         rm = rs.findById(incidence.getIdRoom().getIdRoom()) == null;
 
