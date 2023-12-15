5c31badb220f2af8785a131a8d21bcef276a7ecb
20203tn125 <20203tn125@utez.edu.mx>
2023-12-14 23:59:23 -0600

    [UPDATE] Eliminar cross de front xdsda

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,2 @@
 insert into rol (description,name) values ('Gerente','ROLE_GERENTE'),('Recepcionista','ROLE_RECEPCIONISTA'),('Personal de limpieza','ROLE_LIMPIEZA');
-insert into person(name,surname,lastname) values('Sergio','Cortes','Popoca');
-insert into user(id_person,id_rol,email,password,username,status) values(1,1,'sergiocortes518@gmail.com','$2a$10$DsLoOlL0q4Swr9eNz20HeuaDTbraVfe90OKsGjJir/t7Dpuh55sZ2','serch21',1);
+
