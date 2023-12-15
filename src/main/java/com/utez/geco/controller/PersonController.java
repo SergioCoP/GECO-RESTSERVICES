761bef4e654d59e5c8695b552c957c8ac209db04
20203tn125 <20203tn125@utez.edu.mx>
2023-12-15 01:13:27 -0600

    [FIX] Ya por favor

--- src/main/java/com/utez/geco/controller/PersonController.java
+++ src/main/java/com/utez/geco/controller/PersonController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.List;
 
-@CrossOrigin(origins = "http://52.1.80.209:3000", methods = {RequestMethod.OPTIONS, RequestMethod.GET, RequestMethod.DELETE, RequestMethod.PUT, RequestMethod.POST},allowedHeaders = {HttpHeaders.CONTENT_TYPE,HttpHeaders.AUTHORIZATION})
+@CrossOrigin(origins = "http://52.1.80.209:3000", methods = {RequestMethod.OPTIONS, RequestMethod.GET, RequestMethod.DELETE, RequestMethod.PUT, RequestMethod.POST},allowedHeaders = {HttpHeaders.CONTENT_TYPE,HttpHeaders.AUTHORIZATION},exposedHeaders = {HttpHeaders.CONTENT_TYPE,HttpHeaders.AUTHORIZATION})
 @RequestMapping(value = "/api/person")
 @RestController
 public class PersonController {
